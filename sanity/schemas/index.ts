edc1d58d3b8cfc0bc439efabf005e528d602b3a2
bick27123 <bick27123@gmail.com>
2026-03-07 19:09:57 -0400

    Remove testimonial schema from Sanity Studio

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,7 +1,6 @@
 import project from './project'
 import service from './service'
-import testimonial from './testimonial'
 import hero from './hero'
 import companyInfo from './companyInfo'
 
-export const schemaTypes = [project, service, testimonial, hero, companyInfo]
+export const schemaTypes = [project, service, hero, companyInfo]
